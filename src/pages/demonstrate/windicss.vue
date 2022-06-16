5cfab2fa966145d1dd683623abdbe566d1546899
LiuYang <fyeeme@gmail.com>
2022-06-16 21:34:06 +0800

    fix: import

--- src/pages/demonstrate/windicss.vue
+++ src/pages/demonstrate/windicss.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="m-4 bg-gray-200 py-4">
-    <span>windicss</span>
+    <span class="text-sm px-4">windicss</span>
     <div class="border-b-2 border-dark-200 border-dashed">border</div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { reactive } from '@vue/reactivity'
-  import { onMounted, ref, watch } from 'vue'
+  import { onMounted, reactive, ref, watch } from 'vue'
   import { userApi } from 'src/api/user'
   import { useRoute, useRouter } from 'vue-router'
 
@@ -38,4 +37,13 @@
   }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .body {
+    padding: 15px;
+    margin: 20px;
+    border: 1px solid #ffff;
+    background-color: #000;
+    width: 200px;
+    height: 300px;
+  }
+</style>
