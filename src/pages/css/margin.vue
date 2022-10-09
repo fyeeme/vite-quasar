50f401dc0af66d4fb077f82498c293bae4e187f7
LiuYang <fyeeme@gmail.com>
2022-10-09 11:04:13 +0800

    feat: add windicss

---
+++ src/pages/css/margin.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="bg-gray-50 h-20 mb-6"></section>
+  <section class="bg-blue-50 h-20 mt-4"></section>
+  <div class="border-t border-stone-400 my-2"></div>
+  <!-- margin-top 折叠了 -->
+  <section class="bg-pink-50 h-20 mt-6 py-1">
+    <div class="bg-blue-100 h-15 mt-6"></div>
+  </section>
+
+  <!-- margin-top 没有折叠了 ，我们加了 padding-->
+  <section class="bg-pink-50 h-20 mt-6">
+    <div class="bg-blue-100 h-15 mt-6"></div>
+  </section>
+
+  <section class="bg-pink-50 h-20 mt-6">
+    <div class="bg-blue-100 mt-6"></div>
+    <div class="bg-blue-100 mt-6"></div>
+    <div class="bg-blue-100 mt-6"></div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped></style>
